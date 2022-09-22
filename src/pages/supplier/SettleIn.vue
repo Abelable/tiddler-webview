8a32f7023a95a80add7d135af6b2a3525c445b13
Administrator <admin@example.com>
2022-09-22 15:02:40 +0800

    chore: 商家入驻

---
+++ src/pages/supplier/SettleIn.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>商家入驻</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/router/routes/home.ts
+++ src/router/routes/home.ts
@@ -1,11 +1,14 @@
 import { RouteRecordRaw } from "vue-router";
 
-const home: Array<RouteRecordRaw> = [
+const supplier: Array<RouteRecordRaw> = [
   {
-    path: "/mall",
-    name: "mall",
-    component: () => import("@/pages/home/subpages/mall/index.vue"),
+    path: "/supplier/settle_in",
+    name: "supplier_settle_in",
+    meta: {
+      title: "商家入驻",
+    },
+    component: () => import("@/pages/supplier/SettleIn.vue"),
   },
 ];
 
-export default home;
+export default supplier;
