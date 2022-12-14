de60f8a88f28080bb7fce521c77a7c52b5b7222b
Administrator <admin@example.com>
2022-12-14 09:08:23 +0800

    feat: ppt

--- src/pages/merchant/SettleIn.vue
+++ src/pages/merchant/SettleIn.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <div class="step-one" v-if="step === 0">
+    <div class="home" v-show="step === 0">
       <div class="main">
         <div class="title">商家入驻</div>
-        <div class="second-title">请选择店铺类型:</div>
+        <div class="sub-title">请选择店铺类型:</div>
         <div class="selection">
           <div
             class="option"
@@ -38,7 +38,7 @@
         </div>
       </div>
     </div>
-    <div class="information-filling" v-else>
+    <div class="information-filling" v-show="step !== 0">
       <div class="header">
         <div class="title">信息填写</div>
         <div class="sub-title">真实有效的信息有助于您快速通过审核</div>
@@ -46,10 +46,10 @@
           <div
             class="step"
             v-for="(item, index) in [
-              '实体店铺信息',
-              '个人身份信息',
-              '银行身份信息',
-              '小店信息',
+              '店铺信息',
+              '个人信息',
+              '银行信息',
+              '其他信息',
             ]"
             :key="index"
           >
@@ -70,6 +70,65 @@
         </div>
       </div>
       <div class="main">
+        <div class="part" v-show="step === 1">
+          <div class="title">请输入实体店铺相关信息</div>
+          <div class="form-wrap">
+            <div class="form-item">
+              <div class="form-title">店铺名称</div>
+              <input
+                class="input"
+                type="text"
+                placeholder="请输入实体店铺名称"
+              />
+            </div>
+            <div class="form-item">
+              <div class="form-title">联系地址</div>
+              <div
+                class="area-picker"
+                :class="{ active: pickedAreaDesc }"
+                @click="areaPickerPopupVisible = true"
+              >
+                {{ pickedAreaDesc || "请选择省、市、区" }}
+              </div>
+              <Popup
+                v-model:show="areaPickerPopupVisible"
+                position="bottom"
+                round
+              >
+                <Area
+                  :area-list="areaList"
+                  @confirm="areaConfirm"
+                  @cancel="areaPickerPopupVisible = false"
+                />
+              </Popup>
+            </div>
+            <div class="form-item">
+              <div class="form-title">详细地址</div>
+              <input class="input" type="text" placeholder="请输入详细地址" />
+            </div>
+            <div class="form-item">
+              <div class="form-title">联系电话</div>
+              <input class="input" type="tel" placeholder="请输入联系电话" />
+            </div>
+            <div class="form-item">
+              <div class="form-title">电子邮箱</div>
+              <input class="input" type="email" placeholder="请输入电子邮箱" />
+            </div>
+          </div>
+        </div>
+        <div class="part" v-show="step === 2">
+          <div class="title">请上传个人身份相关信息</div>
+          <div class="form-wrap">
+            <div class="form-item">
+              <div class="form-title">姓名</div>
+              <input class="input" type="text" placeholder="请输入姓名" />
+            </div>
+            <div class="form-item">
+              <div class="form-title">身份证号</div>
+              <input class="input" type="text" placeholder="请输入身份证号" />
+            </div>
+          </div>
+        </div>
         <div class="btns">
           <div class="btn previous-step" @click="step = step - 1">上一步</div>
           <div class="btn next-step" @click="step = step + 1">
@@ -82,21 +141,41 @@
 </template>
 
 <script setup lang="ts">
-import { Checkbox } from "vant";
+import { Checkbox, Area, Popup } from "vant";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { areaList } from "@vant/area-data";
 
 const router = useRouter();
-const step = ref(0);
+const step = ref(1);
 const agreementsChecked = ref(false);
 const curTypeOptionIdx = ref(-1);
+const areaPickerPopupVisible = ref(false);
+const pickedArea = ref<string[]>([]);
+const pickedAreaDesc = ref("");
 
+interface RegionOption {
+  text: string;
+  value: string;
+  children?: RegionOption[];
+}
+const areaConfirm = ({
+  selectedValues,
+  selectedOptions,
+}: {
+  selectedValues: string[];
+  selectedOptions: RegionOption[];
+}) => {
+  pickedArea.value = selectedValues;
+  pickedAreaDesc.value = `${selectedOptions[0].text} ${selectedOptions[1].text} ${selectedOptions[2].text}`;
+  areaPickerPopupVisible.value = false;
+};
 const checkAgreement = () => router.push("/agreement/merchant_agreement");
 </script>
 
 <style lang="scss" scoped>
 .container {
-  .step-one {
+  .home {
     display: flex;
     flex-direction: column;
     padding-top: 3.5rem;
@@ -114,10 +193,10 @@ const checkAgreement = () => router.push("/agreement/merchant_agreement");
       border-radius: 0.24rem 0.24rem 0 0;
       .title {
         color: #333;
-        font-size: 0.36rem;
+        font-size: 0.5rem;
         font-weight: 550;
       }
-      .second-title {
+      .sub-title {
         margin-top: 0.1rem;
         color: #666;
         font-size: 0.28rem;
@@ -191,7 +270,7 @@ const checkAgreement = () => router.push("/agreement/merchant_agreement");
       .title {
         padding: 0 0.24rem;
         color: #222;
-        font-size: 0.36rem;
+        font-size: 0.42rem;
         font-weight: 550;
       }
       .sub-title {
@@ -236,7 +315,56 @@ const checkAgreement = () => router.push("/agreement/merchant_agreement");
       margin-top: 0.48rem;
       flex: 1;
       border-radius: 0.32rem 0.32rem 0 0;
+      overflow: hidden;
       background: #fff;
+      .part {
+        display: flex;
+        flex-direction: column;
+        padding-bottom: 1.52rem;
+        height: 100%;
+        .title {
+          padding: 0.32rem;
+          color: #333;
+          font-size: 0.32rem;
+          font-weight: 550;
+          background: #fff;
+        }
+        .form-wrap {
+          padding: 0 0.32rem;
+          flex: 1;
+          overflow-y: scroll;
+          .form-item {
+            margin-bottom: 0.32rem;
+            font-size: 0;
+            .form-title {
+              padding-left: 0.12rem;
+              color: #a3a3a3;
+              font-size: 0.26rem;
+              font-weight: 550;
+            }
+            .input,
+            .area-picker {
+              margin-top: 0.12rem;
+              padding: 0 0.24rem;
+              height: 1rem;
+              font-size: 0.26rem;
+              border-radius: 0.24rem;
+              background: #e9f7ee;
+            }
+            .input {
+              width: 100%;
+            }
+            .area-picker {
+              display: flex;
+              align-items: center;
+              color: #777;
+              &.active {
+                color: #333;
+              }
+            }
+          }
+        }
+      }
       .btns {
         position: absolute;
         left: 0;
