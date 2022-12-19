4c216f0fc72c22afa7ea948f757484530173a123
Administrator <admin@example.com>
2022-12-19 14:05:10 +0800

    feat: 运费模板

--- src/pages/merchant/CreateFreightTemplate.vue
+++ src/pages/merchant/CreateFreightTemplate.vue
@@ -14,7 +14,7 @@
         </div>
       </div>
     </div>
-    <div class="main">
+    <div class="main" v-if="templateType === 0">
       <div class="title">基础信息</div>
       <div class="card">
         <div class="form">
@@ -50,7 +50,7 @@
           class="add-btn"
           @click="addArea"
           icon="plus"
-          text="新增"
+          text="添加"
           type="primary"
           size="mini"
         />
@@ -66,7 +66,8 @@
             <div class="form-item">
               <div class="label">地区</div>
               <div class="picker" :class="{ active: item.pickedAreaDesc }">
-                {{ item.pickedAreaDesc || "请选择地区" }}
+                <div>{{ item.pickedAreaDesc || "请选择地区" }}</div>
+                <Icon name="arrow" />
               </div>
             </div>
             <div class="form-item">
@@ -84,6 +85,135 @@
           <Button class="delete-btn" icon="delete" color="#EE0D23" plain />
         </template>
       </SwipeCell>
+      <div class="title">
+        <div>快递方式</div>
+        <Button
+          class="add-btn"
+          @click="addExpress"
+          icon="plus"
+          text="添加"
+          type="primary"
+          size="mini"
+        />
+      </div>
+      <SwipeCell
+        :before-close="deleteExpress"
+        v-for="(item, index) in expressList"
+        :key="index"
+        :name="index"
+      >
+        <div class="card">
+          <div class="form">
+            <div class="form-item">
+              <div class="label">快递名称</div>
+              <div class="picker" :class="{ active: item.pickedExpressDesc }">
+                <div>{{ item.pickedExpressDesc || "请选择快递" }}</div>
+                <Icon name="arrow" />
+              </div>
+            </div>
+            <div class="form-item">
+              <div class="label">额外运费</div>
+              <input
+                class="input"
+                v-model="expressList[index].fee"
+                type="number"
+                placeholder="例：0.00"
+              />
+            </div>
+          </div>
+        </div>
+        <template #right>
+          <Button class="delete-btn" icon="delete" color="#EE0D23" plain />
+        </template>
+      </SwipeCell>
+    </div>
+    <div class="main" v-if="templateType === 1">
+      <div class="title">
+        <div>基础信息</div>
+        <Button
+          class="add-btn"
+          @click="addArea"
+          icon="plus"
+          text="添加快递模板"
+          type="primary"
+          size="mini"
+        />
+      </div>
+      <div class="card">
+        <div class="form">
+          <div class="form-item">
+            <div class="label">模板名称</div>
+            <input class="input" type="text" placeholder="请输入模板名称" />
+          </div>
+          <div class="form-item">
+            <div class="label">运费标题</div>
+            <input class="input" type="text" placeholder="请输入运费标题" />
+          </div>
+        </div>
+      </div>
+      <div class="title">
+        <div>顺丰快递</div>
+        <Button
+          class="add-btn"
+          @click="addArea"
+          icon="plus"
+          text="添加"
+          type="primary"
+          size="mini"
+        />
+      </div>
+      <SwipeCell
+        :before-close="deleteArea"
+        v-for="(item, index) in areaList"
+        :key="index"
+        :name="index"
+      >
+        <div class="card">
+          <div class="form">
+            <div class="form-item">
+              <div class="label">区域名称</div>
+              <input class="input" type="text" placeholder="请输入区域名称" />
+            </div>
+            <div class="form-item">
+              <div class="label">费用计算方式</div>
+              <RadioGroup
+                class="selection"
+                direction="horizontal"
+                icon-size="0.32rem"
+              >
+                <Radio name="1">按重量</Radio>
+                <Radio name="2">按商品件数</Radio>
+              </RadioGroup>
+            </div>
+            <div class="form-item">
+              <div class="label">首重1KG以内费用</div>
+              <input class="input" type="number" placeholder="例：0.00" />
+            </div>
+            <div class="form-item">
+              <div class="label">续重每1KG或其零数的费用</div>
+              <input class="input" type="number" placeholder="例：0.00" />
+            </div>
+            <div class="form-item">
+              <div class="label">单件商品费用</div>
+              <input class="input" type="number" placeholder="例：0.00" />
+            </div>
+            <div class="form-item">
+              <div class="label">免费额度</div>
+              <input class="input" type="number" placeholder="例：0.00" />
+            </div>
+            <div class="form-item">
+              <div class="label">目的地</div>
+              <div class="picker" :class="{ active: item.pickedAreaDesc }">
+                <div>{{ item.pickedAreaDesc || "请选择目的地" }}</div>
+                <Icon name="arrow" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <template #right>
+          <Button class="delete-btn" icon="delete" color="#EE0D23" plain />
+        </template>
+      </SwipeCell>
     </div>
     <div class="btns">
       <button class="save-btn">保存</button>
@@ -92,11 +222,19 @@
 </template>
 
 <script setup lang="ts">
-import { RadioGroup, Radio, SwipeCell, showConfirmDialog, Button } from "vant";
+import {
+  RadioGroup,
+  Radio,
+  SwipeCell,
+  showConfirmDialog,
+  Button,
+  Icon,
+} from "vant";
 import { ref, reactive } from "vue";
 const templateType = ref(0);
 const computeType = ref(1);
 const areaList = reactive([{ pickedAreaDesc: "", fee: "" }]);
+const expressList = reactive([{ pickedExpressDesc: "", fee: "" }]);
 
 const addArea = () => areaList.push({ pickedAreaDesc: "", fee: "" });
 const deleteArea = (res: any) => {
@@ -104,6 +242,19 @@ const deleteArea = (res: any) => {
     showConfirmDialog({ title: "确定删除该配送地区配置吗？" })
       .then(() => {
         areaList.splice(res.name, 1);
+        return true;
+      })
+      .catch(() => true);
+  } else {
+    return true;
+  }
+};
+const addExpress = () => expressList.push({ pickedExpressDesc: "", fee: "" });
+const deleteExpress = (res: any) => {
+  if (res.position === "right") {
+    showConfirmDialog({ title: "确定删除该快递方式配置吗？" })
+      .then(() => {
+        expressList.splice(res.name, 1);
         return true;
       })
       .catch(() => true);
@@ -151,7 +302,7 @@ const deleteArea = (res: any) => {
     padding: 0.32rem;
     flex: 1;
     overflow-y: scroll;
-    border-radius: 0.32rem;
+    border-radius: 0.32rem 0.32rem 0 0;
     background: #fff;
     .title {
       display: flex;
@@ -167,38 +318,42 @@ const deleteArea = (res: any) => {
       padding: 0 0.32rem;
       background: #eaf6ff;
       border-radius: 0.32rem;
+      .form {
+        .form-item {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          height: 1rem;
+          border-bottom: 1px solid #e8e8e8;
+          &:last-child {
+            border-bottom: none;
+          }
+          .label {
+            color: #333;
+            font-size: 0.26rem;
+          }
+          .input,
+          .selection,
+          .picker {
+            font-size: 0.26rem;
+          }
+          .input {
+            text-align: right;
+          }
+          .picker {
+            display: flex;
+            align-items: center;
+            color: #777;
+            &.active {
+              color: #333;
+            }
+          }
+        }
+      }
     }
     .delete-btn {
       height: 100%;
       border: none;
-    }
-    .form {
-      .form-item {
-        display: flex;
-        align-items: center;
-        height: 1rem;
-        border-bottom: 1px solid #e8e8e8;
-        &:last-child {
-          border-bottom: none;
-        }
-        .label {
-          flex: 1;
-          color: #333;
-          font-size: 0.26rem;
-        }
-        .input,
-        .selection,
-        .picker {
-          flex: 3;
-          font-size: 0.26rem;
-        }
-        .picker {
-          color: #777;
-          &.active {
-            color: #333;
-          }
-        }
-      }
     }
   }
   .btns {
