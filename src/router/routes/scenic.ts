713d38d8f9b65fedb0e97d5d64195840132b5dc3
Administrator <admin@example.com>
2023-06-05 22:30:01 +0800

    feat: 景区服务商入驻

--- src/router/routes/scenic.ts
+++ src/router/routes/scenic.ts
@@ -2,20 +2,20 @@ import { RouteRecordRaw } from "vue-router";
 
 const scenic: Array<RouteRecordRaw> = [
   {
-    path: "/scenic/agreements/merchant_service",
-    name: "scenic_merchant_service",
+    path: "/scenic/agreements/provider_service",
+    name: "scenic_provider_service",
     meta: {
       title: "景区服务商服务协议",
     },
-    component: () => import("@/pages/scenic/agreements/merchantService.vue"),
+    component: () => import("@/pages/scenic/agreements/providerService.vue"),
   },
   {
-    path: "/scenic/merchant/settle_in",
-    name: "scenic_merchant_settle_in",
+    path: "/scenic/provider/settle_in",
+    name: "scenic_provider_settle_in",
     meta: {
       title: "景区服务商入驻",
     },
-    component: () => import("@/pages/scenic/merchant/settleIn.vue"),
+    component: () => import("@/pages/scenic/provider/settleIn.vue"),
   },
 ];
 
