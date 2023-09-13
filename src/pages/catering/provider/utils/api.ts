6911d60e8357649662d0db3cc6aebcb34959fcbb
Administrator <admin@example.com>
2023-09-13 10:58:35 +0800

    feat: 餐馆商家入驻

--- src/pages/catering/provider/utils/api.ts
+++ src/pages/catering/provider/utils/api.ts
@@ -6,20 +6,20 @@ import {
 } from "./type";
 
 export const uploadProviderInfo = async (info: CreateProviderInfo) =>
-  await http("food/provider/settle_in", { method: "POST", data: info });
+  await http("catering/provider/settle_in", { method: "POST", data: info });
 
 export const getRestaurantCategoryOptions = async (): Promise<
   RestaurantCategoryOption[]
-> => await http("food/restaurant/category_options");
+> => await http("catering/restaurant/category_options");
 
 export const getProviderStatusInfo = async (): Promise<ProviderStatusInfo> =>
-  await http("food/provider/status");
+  await http("catering/provider/status");
 
 export const payProviderDeposit = async (orderId: number) =>
-  await http("food/provider/pay_deposit", {
+  await http("catering/provider/pay_deposit", {
     method: "POST",
     data: { orderId },
   });
 
 export const deleteProvider = async () =>
-  await http("food/provider/delete", { method: "POST" });
+  await http("catering/provider/delete", { method: "POST" });
