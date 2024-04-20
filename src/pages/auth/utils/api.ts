48c0d0667b6cfb6fb0101a14df316bb2656b276e
Administrator <admin@example.com>
2024-04-20 15:07:02 +0800

    feat: 更改用户授权api

--- src/pages/auth/utils/api.ts
+++ src/pages/auth/utils/api.ts
@@ -2,16 +2,16 @@ import { http } from "@/utils/http";
 import { AuthInfo } from "./type";
 
 export const getAuthInfo = async (): Promise<AuthInfo> =>
-  await http("auth_info/detail");
+  await http("auth/info/detail");
 
 export const addAuthInfo = async (authInfo: Partial<AuthInfo>) =>
-  await http("auth_info/add", {
+  await http("auth/info/add", {
     method: "POST",
     data: authInfo,
   });
 
 export const editAuthInfo = async (authInfo: Partial<AuthInfo>) =>
-  await http("auth_info/edit", {
+  await http("auth/info/edit", {
     method: "POST",
     data: authInfo,
   });
