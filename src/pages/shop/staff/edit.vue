e10d3f9c7744ba1779e9aa820f785c4abbe49692
funguy <1209683835@qq.com>
2025-06-18 18:16:24 +0800

    feat: 保证金

--- src/pages/shop/staff/edit.vue
+++ src/pages/shop/staff/edit.vue
@@ -1,9 +1,5 @@
 <template>
-  <Info
-    :editingAddressInfo="editingAddressInfo"
-    @save="save"
-    @delete="_delete"
-  />
+  <Info :editingStaffInfo="editingStaffInfo" @save="save" @delete="_delete" />
 </template>
 
 <script setup lang="ts">
@@ -11,28 +7,28 @@ import Info from "./components/Info.vue";
 import { ref, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { showConfirmDialog, showToast } from "vant";
-import { editAddress, getAddress, deleteAddress } from "./utils/api";
-import type { AddressDetail } from "./utils/type";
+import { editStaff, getStaff, deleteStaff } from "./utils/api";
+import type { StaffDetail } from "./utils/type";
 
 const route = useRoute();
 const router = useRouter();
 
 const addressId = ref(0);
-const editingAddressInfo = ref<Omit<AddressDetail, "id">>();
+const editingStaffInfo = ref<Omit<StaffDetail, "id">>();
 
 onMounted(async () => {
-  const { id, ...rest } = await getAddress(+(route.query.id as string));
+  const { id, ...rest } = await getStaff(+(route.query.id as string));
   addressId.value = id;
-  editingAddressInfo.value = rest;
+  editingStaffInfo.value = rest;
 });
 
 const save = async ({
   addressInfo,
 }: {
-  addressInfo: Omit<AddressDetail, "id">;
+  addressInfo: Omit<StaffDetail, "id">;
 }) => {
   try {
-    await editAddress(addressInfo);
+    await editStaff(addressInfo);
     router.back();
   } catch (error) {
     showToast("保存失败，请重试");
@@ -43,7 +39,7 @@ const _delete = () =>
   showConfirmDialog({ title: "确定删除该退货地址吗？" })
     .then(async () => {
       try {
-        await deleteAddress(addressId.value);
+        await deleteStaff(addressId.value);
         router.back();
       } catch (error) {
         showToast("删除失败，请重试");
