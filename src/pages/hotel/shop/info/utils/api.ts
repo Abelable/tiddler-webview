5d566d1ff6b6ae18084e2cc6f68ec441c02a5d5e
funguy <1209683835@qq.com>
2025-07-15 17:46:44 +0800

    feat: 酒店相关

--- src/pages/hotel/shop/info/utils/api.ts
+++ src/pages/hotel/shop/info/utils/api.ts
@@ -3,11 +3,11 @@ import { cleanObject } from "@/utils";
 import type { ShopInfo } from "./type";
 
 export const getShopInfo = async (id: number): Promise<ShopInfo> => {
-  return await http("scenic/shop/info", { data: { id } });
+  return await http("hotel/shop/info", { data: { id } });
 };
 
 export const updateShopInfo = async (info: Partial<ShopInfo>) =>
-  await http("scenic/shop/update_info", {
+  await http("hotel/shop/update_info", {
     method: "POST",
     data: cleanObject(info),
   });
--- src/pages/hotel/shop/manager/utils/api.ts
+++ src/pages/hotel/shop/manager/utils/api.ts
@@ -3,34 +3,34 @@ import { http } from "@/utils/http";
 import type { Manager } from "./type";
 
 export const getManagerList = async (shopId: number): Promise<Manager[]> =>
-  await http("scenic/shop/manager/list", { data: { shopId } });
+  await http("hotel/shop/manager/list", { data: { shopId } });
 
 export const getManager = async (
   shopId: number,
   id: number
 ): Promise<Manager> =>
-  await http("scenic/shop/manager/detail", { data: { shopId, id } });
+  await http("hotel/shop/manager/detail", { data: { shopId, id } });
 
 export const createManager = async (
   shopId: number,
   userId: number,
   roleId: number,
-  scenicIds: number[]
+  hotelIds: number[]
 ) =>
-  await http("scenic/shop/manager/add", {
+  await http("hotel/shop/manager/add", {
     method: "POST",
-    data: { shopId, userId, roleId, scenicIds },
+    data: { shopId, userId, roleId, hotelIds },
   });
 
 export const editManager = async (
   shopId: number,
   id: number,
   roleId: number,
-  scenicIds: number[]
+  hotelIds: number[]
 ) =>
-  await http("scenic/shop/manager/edit", {
+  await http("hotel/shop/manager/edit", {
     method: "POST",
-    data: { shopId, id, roleId, scenicIds },
+    data: { shopId, id, roleId, hotelIds },
   });
 
 export const deleteManager = async (shopId: number, id: number) =>
